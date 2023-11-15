b996d60e3f56e817561a693831d4fb4475bb1ddc
hirosuke0520 <hirosuke.creator@gmail.com>
2023-11-15 14:49:14 +0900

    1ファイルに戻す

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify
-from tiktok_scraping import get_tiktok_profile_by_selenium
+# from tiktok_scraping import get_tiktok_profile_by_selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 app = Flask(__name__)
 
@@ -11,7 +15,22 @@ def index():
     if args_user_ids:
         user_ids = args_user_ids.split(',')  # カンマ区切りのuser_idを分割
     
-    profiles = [get_tiktok_profile_by_selenium(user_id) for user_id in user_ids]
+    # profiles = [get_tiktok_profile_by_selenium(user_id) for user_id in user_ids]
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-dev-shm-usage')
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+
+    profiles = []
+    for user_id in user_ids:
+        URL = 'https://tonari-it.com/scraping-test/'
+        driver.get(URL)
+        driver.implicitly_wait(5)
+        element = driver.find_element(By.CSS_SELECTOR, "#hoge")
+        profiles.append({user_id: user_id})
 
     return jsonify(profiles)
 
